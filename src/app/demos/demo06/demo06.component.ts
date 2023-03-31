6b526d005f4b34bfad260087bac7b9a6a02912c9
Quentin <quentin.geerts@bstorm.be>
2023-03-31 09:54:15 +0200

    Demo 06 - Les Pipes

--- src/app/demos/demo06/demo06.component.ts
+++ src/app/demos/demo06/demo06.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import localeFr from '@angular/common/locales/fr';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr');
 
 @Component({
   selector: 'app-demo06',
@@ -8,6 +13,10 @@ import { Component } from '@angular/core';
 export class Demo06Component {
 
   maVariable1: string = "Hello TOUT le monde !";
-  maVariable2: number = 12345.6789
+  maVariable2: number = 12345.6789;
+  maVariable3: Date = new Date();
+  maVariable4: number = 0.8656325;
+  maVariable5: string[] = ["Poivre", "Sel", "Sucre", "Epices"];
+  maVariable6: {} = { nom: "Geerts", prenom: "Quentin" };
 
 }
