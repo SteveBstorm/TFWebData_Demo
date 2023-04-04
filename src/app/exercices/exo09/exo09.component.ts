af95ff8c8a12f6b660ac4cd43a314262ee3eaac8
Samuel Legrain <samuel.legrain@bstorm.be>
2023-04-04 10:41:36 +0200

    Add correctif Exo09

--- src/app/exercices/exo09/exo09.component.ts
+++ src/app/exercices/exo09/exo09.component.ts
@@ -10,4 +10,46 @@ export class Exo09Component {
 
   article: Article = { name: 'Poire', qty: 1, dateAdd: new Date() }
 
+  panier: Article[] = [];
+
+  name_product! : string;
+  qty_product : number;
+
+  constructor(){
+    this.qty_product = 1;
+  }
+
+  addItem():void{
+    if(!this.name_product || this.name_product.trim().length <= 0) return;
+    if(this.qty_product < 1) return;
+    let index : number = this.panier.findIndex(a => a.name ==this.name_product);
+    if( index < 0)
+      this.panier.push(
+        { name: this.name_product.trim(), qty: this.qty_product, dateAdd: new Date() }
+      );
+    else
+        this.panier[index].qty += this.qty_product;
+    this.name_product = "";
+    this.qty_product = 1;
+
+    const html_input_name = document.getElementById("name_product");
+    html_input_name?.focus();
+  }
+
+  delItem(article : Article):void{
+    this.panier = this.panier.filter(a => a != article);
+  }
+
+  delItemWithIndex(index : number):void{
+    this.panier.splice(index,1);
+  }
+
+  lessItem(index : number):void{
+    this.panier[index].qty --;
+    if(this.panier[index].qty <= 0) this.delItemWithIndex(index);
+  }
+
+  moreItem(index : number):void{
+    this.panier[index].qty ++;
+  }
 }
